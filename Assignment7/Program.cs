691fbad496a1b2a4a06cd88635546caf7caaf6b8
Akshaypatil1820 <65176871+Akshaypatil1820@users.noreply.github.com>
2020-12-10 10:22:01 +0530

    Add files via upload

---
+++ Assignment7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal, decimal, decimal> o = (p, n, r) => p * n * r / 100;
+
+            Func<int, int, bool> o1 = (a, b) => a > b;
+
+            Func<Employee, decimal> o3 = emp => emp.Basic;
+
+            Predicate<int> o4 = x => x % 2 == 0;
+
+            Predicate<Employee> o5 = emp => emp.Basic > 10000;
+        }
+    }
+}
